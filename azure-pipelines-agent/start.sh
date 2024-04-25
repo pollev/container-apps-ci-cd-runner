ef120a4e63486a80f1b81094bf2c4a7d9c1382fd
Polle <vanhoofpolle@gmail.com>
2024-04-25 16:50:45 +0200

    support sp auth

--- azure-pipelines-agent/start.sh
+++ azure-pipelines-agent/start.sh
@@ -7,16 +7,27 @@ if [ -z "$AZP_URL" ]; then
 fi
 
 if [ -z "$AZP_TOKEN_FILE" ]; then
-  if [ -z "$AZP_TOKEN" ]; then
-    echo 1>&2 "error: missing AZP_TOKEN environment variable"
+  if [ -z "$AZP_TOKEN" ] && [[ -z $AZP_SP_CLIENTID || -z $AZP_SP_SECRET || -z $AZP_SP_TENANTID ]]; then
+    echo 1>&2 "error: missing AZP_TOKEN or (AZP_SP_CLIENTID+AZP_SP_SECRET+AZP_SP_TENANTID) environment variable(s)"
     exit 1
   fi
 
   AZP_TOKEN_FILE=/azp/.token
-  echo -n $AZP_TOKEN > "$AZP_TOKEN_FILE"
+  AZP_SP_SECRET_FILE=/azp/.sp_secret
+  if [[ -z $AZP_SP_CLIENTID || -z $AZP_SP_SECRET || -z $AZP_SP_TENANTID ]]; then
+    echo -n $AZP_TOKEN > "$AZP_TOKEN_FILE"
+    AZP_AUTH_TYPE="pat"
+  else
+    az login --service-principal -u "$AZP_SP_CLIENTID" -p "$AZP_SP_SECRET" --tenant "$AZP_SP_TENANTID" --allow-no-subscriptions
+    AZP_SP_TOKEN=$(az account get-access-token --resource 499b84ac-1321-427f-aa17-267ca6975798 --query "accessToken" --output tsv)
+    echo -n $AZP_SP_TOKEN > "$AZP_TOKEN_FILE"
+    echo -n $AZP_SP_SECRET > "$AZP_SP_SECRET_FILE"
+    AZP_AUTH_TYPE="sp"
+  fi
 fi
 
 unset AZP_TOKEN
+unset AZP_SP_SECRET
 
 if [ -n "$AZP_WORK" ]; then
   mkdir -p "$AZP_WORK"
@@ -36,7 +47,11 @@ cleanup() {
     # If the agent has some running jobs, the configuration removal process will fail.
     # So, give it some time to finish the job.
     while true; do
-      ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+      if [[ "$AZP_AUTH_TYPE" == "pat" ]]; then
+          ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+      else
+          ./config.sh remove --unattended --auth SP --clientid "$AZP_SP_CLIENTID" --clientsecret $(cat "$AZP_SP_SECRET_FILE") --tenantid "$AZP_SP_TENANTID" && break
+      fi
 
       echo "Retrying in 30 seconds..."
       sleep 30
@@ -80,15 +95,31 @@ trap 'cleanup; exit 143' TERM
 
 print_header "3. Configuring Azure Pipelines agent..."
 
-./config.sh --unattended \
-  --agent "${AZP_AGENT_NAME:-$(hostname)}" \
-  --url "$AZP_URL" \
-  --auth PAT \
-  --token $(cat "$AZP_TOKEN_FILE") \
-  --pool "${AZP_POOL:-Default}" \
-  --work "${AZP_WORK:-_work}" \
-  --replace \
-  --acceptTeeEula & wait $!
+
+if [[ "$AZP_AUTH_TYPE" == "pat" ]]; then
+    ./config.sh --unattended \
+      --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+      --url "$AZP_URL" \
+      --auth PAT \
+      --token $(cat "$AZP_TOKEN_FILE") \
+      --pool "${AZP_POOL:-Default}" \
+      --work "${AZP_WORK:-_work}" \
+      --replace \
+      --acceptTeeEula & wait $!
+else
+    ./config.sh --unattended \
+      --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+      --url "$AZP_URL" \
+      --auth SP \
+      --clientid "$AZP_SP_CLIENTID"
+      --clientsecret $(cat "$AZP_SP_SECRET_FILE")
+      --tenantid "$AZP_SP_TENANTID"
+      --pool "${AZP_POOL:-Default}" \
+      --work "${AZP_WORK:-_work}" \
+      --replace \
+      --acceptTeeEula & wait $!
+fi
+
 
 print_header "4. Running Azure Pipelines agent..."
 
